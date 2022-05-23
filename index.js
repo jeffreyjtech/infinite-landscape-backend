a15c62ea557c5a68ae382977579276c3fc53efbb
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-23 16:30:50 -0700

    Commented out auth stuff temporarily

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const { authSequelize } = require('./server/auth/models');
-const { contentSequelize } = require('./server/models');
-const { start } = require('./server');
+// const { authSequelize } = require('./server/auth/models');
+const { contentDb } = require('./server/models');
+const { start } = require('./server/server');
 const PORT = process.env.PORT || 3000;
 
-authSequelize.sync()
-  .then(() => {
-    console.log('Auth DB is synced');
-  })
-  .catch(console.error);
+// authSequelize.sync()
+//   .then(() => {
+//     console.log('Auth DB is synced');
+//   })
+//   .catch(console.error);
 
-contentSequelize.sync()
+contentDb.sync()
   .then(() => {
     console.log('Content DB is synced');
     start(PORT);
