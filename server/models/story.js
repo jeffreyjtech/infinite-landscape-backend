899eea00e9edbdf439ea8b541abe144b09e1a626
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-23 15:12:23 -0700

    Created story schema

---
+++ server/models/story.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+const genres = ['horror', 'sci-fi', 'fantasy'];
+
+const storySchema = {
+  title: { type: DataTypes.STRING, allowNull: false },
+  username: { type: DataTypes.STRING, allowNull: false },
+  penName: { type: DataTypes.STRING, allowNull: false },
+  storyId: { type: DataTypes.STRING, allowNull: false, unique: true },
+  description: { type: DataTypes.STRING, allowNull: false },
+  category: { type: DataTypes.ENUM(genres), allowNull: false },
+  color: { type: DataTypes.STRING, allowNull: false },
+  tooltips: { type: DataTypes.JSON },
+};
+
+module.exports = storySchema;
