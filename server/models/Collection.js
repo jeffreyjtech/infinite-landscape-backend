f9543c1b37f7a4cd16baa0eb68bd34b551b80a58
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-23 16:29:58 -0700

    Broke up awaits and return statements

--- server/models/Collection.js
+++ server/models/Collection.js
@@ -6,23 +6,28 @@ class Collection {
   }
 
   async create(json) {
-    return await this.model.create(json);
+    const instance = await this.model.create(json);
+    return instance;
   }
 
   async read(id = null) {
-    return (await id) ? this.model.findOne({ where: { id } }) : this.model.findAll();
+    const instance = (await id)
+      ? this.model.findOne({ where: { id } })
+      : this.model.findAll();
+    return instance;
   }
 
   // update method, takes in an id, returns the update instance
   async update(id, json) {
-    await this.model.update(json, { where: id });
-    return await this.read(id);
+    await this.model.update(json, { where: { id } });
+    const instance = await this.read(id);
+    return instance;
   }
 
   // delete method, takes in an id, returns the removed instance
   async delete(id) {
     const removedRecord = await this.read(id);
-    await this.model.destroy({ where: id });
+    await this.model.destroy({ where: { id } });
     return removedRecord;
   }
 }
