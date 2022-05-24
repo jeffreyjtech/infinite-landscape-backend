19f10392a319a16db00e509f6a87745ffeefbad3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-23 20:24:33 -0700

    Added blank routify unit test file

---
+++ server/routify.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('Testing the router constructor', () => {
+
+  it('Has a GET route', () => {
+    expect(true).toBe(false);
+  });
+  it('Has a POST route', () => {
+    expect(true).toBe(false);
+  });
+  it('Has a GET with id route', () => {
+    expect(true).toBe(false);
+  });
+  it('Has a PUT route', () => {
+    expect(true).toBe(false);
+  });
+  it('Has a DELETE route', () => {
+    expect(true).toBe(false);
+  });
+  it('Does not have a DELETE route for profile path', () => {
+    expect(true).toBe(false);
+  });
+
+});
