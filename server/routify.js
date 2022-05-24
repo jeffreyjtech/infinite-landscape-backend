72da27010024bed8811acace07fb87a6fda7ab2e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-23 19:50:29 -0700

    Created route constructor function

--- server/routify.js
+++ server/routify.js
@@ -1,23 +1,90 @@
 'use strict';
 
-const express  = require('express');
-const Collection = require('./models/Collection.js');
+const express = require('express');
 const router = express.Router();
 
 const bearerAuth = require('./auth/middleware/bearer.js');
-const permsAuth = require('./auth/middleware/perms.js');
+const permsBypass = require('./auth/middleware/perms.js');
 
-module.exports = (model, path) => {
+const errorWithStatus = require('./error/errorWithStatus');
 
-  router.post(`/${path}`, bearerAuth, async (req, res, next) => {
-    let records = await Collection.create();
+const errorOnEmptyBody = (req, res, next) => {
+  if (!req.body) {
+    next(errorWithStatus('Missing request body', 400));
+  } else {
+    next();
+  }
+};
+
+const errorOnBadParam = (paramKey) => (req, res, next) => {
+  if (!req.params || !req.params[paramKey]) {
+    next(errorWithStatus(`Missing URL param: ${paramKey}`, 400));
+  } else {
+    next();
+  }
+};
+
+module.exports = (collection, path) => {
+  router.use(bearerAuth);
+
+  router.post(`/${path}`, errorOnEmptyBody, async (req, res, next) => {
+    try {
+      let record = await collection.create(req.body);
+      res.status(201).json(record);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
   });
 
-  // router.get
+  router.get(`/${path}`, async (req, res, next) => {
+    try {
+      let records = await collection.readAll();
+      res.status(200).json(records);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
 
-  // router.get with id
+  router.get(`/${path}/:id`, errorOnBadParam('id'), async (req, res, next) => {
+    try {
+      let record = await collection.read(req.params.id);
+      res.status(200).json(record);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
 
-  // router.put
+  router.put(
+    `/${path}/:id`,
+    errorOnBadParam('id'),
+    errorOnEmptyBody,
+    permsBypass('admin'),
+    async (req, res, next) => {
+      try {
+        let record = await collection.update(req.params.id, req.body);
+        res.status(200).json(record);
+      } catch (e) {
+        console.error(e);
+        next(e);
+      }
+    },
+  );
 
-  // router.delete
+  router.delete(
+    `/${path}/:id`,
+    errorOnBadParam('id'),
+    permsBypass('admin'),
+    async (req, res, next) => {
+      try {
+        let record = await collection.delete(req.params.id);
+        res.status(200).json(record);
+      } catch (e) {
+        console.error(e);
+        next(e);
+      }
+    },
+  );
 };
