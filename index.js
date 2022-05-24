6e1ed0817585af0fea167315f87523843be75087
Micha Davis <m.leigh.davis@gmail.com>
2022-05-24 11:50:46 -0700

    add auth db initialize

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// const { authSequelize } = require('./server/auth/models');
+const { authDb } = require('./server/auth/models');
 const { contentDb } = require('./server/models');
 const { start } = require('./server/server');
 const PORT = process.env.PORT || 3000;
 
-// authSequelize.sync()
-//   .then(() => {
-//     console.log('Auth DB is synced');
-//   })
-//   .catch(console.error);
+authDb.sync()
+  .then(() => {
+    console.log('Auth DB is synced');
+  })
+  .catch(console.error);
 
 contentDb.sync()
   .then(() => {
