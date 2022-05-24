5d859e82214eb2328d0f41d8bb76a5fcb8caeac0
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-24 15:21:07 -0700

    Fixed conditional test db assignment

--- server/auth/models/index.js
+++ server/auth/models/index.js
@@ -6,15 +6,13 @@ const userModel = require('./users');
 
 const { HEROKU_POSTGRESQL_GOLD_URL, NODE_ENV } = process.env;
 
-const DATABASE_URL = NODE_ENV === 'test' ? HEROKU_POSTGRESQL_GOLD_URL : DATABASE_URL;
+const DATABASE_URL = NODE_ENV === 'test' ? DATABASE_URL : HEROKU_POSTGRESQL_GOLD_URL;
 
 // This will assign the Heroku-specific configs if the database is deployed.
 const config =
   NODE_ENV !== 'test' && DATABASE_URL
     ? { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } } }
-    : { /*logging: false*/ };
-
-
+    : {/*logging: false*/};
 
 const dbUrl = DATABASE_URL || 'postgresql://localhost:5432';
 
