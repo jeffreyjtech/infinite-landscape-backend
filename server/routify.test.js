d7a90c59b5c92b936fbc99bd91647b62d9529ef0
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-24 10:45:11 -0700

    Got preliminary test-of-life to work

--- server/routify.test.js
+++ server/routify.test.js
@@ -1,9 +1,38 @@
 'use strict';
 
+const routify = require('./routify');
+
+const router = {
+  post: jest.fn(),
+  get: jest.fn(),
+  put: jest.fn(),
+  delete: jest.fn(),
+};
+
+const testCollection = {
+  read: jest.fn(),
+  readAll: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
+
+const req = {
+  params: { id: 1 },
+  body: { test: 'test-body' },
+};
+
+const res = jest.fn();
+const next = jest.fn();
+
 describe('Testing the router constructor', () => {
+  const newRoute = routify(testCollection, 'test', router);
+  const calls = router.get.mock.calls;
 
   it('Has a GET route', () => {
-    expect(true).toBe(false);
+    const middleware = calls[0][1];
+    console.log(middleware(req, res, next));
+    expect().toHaveBeenCalled();
   });
   it('Has a POST route', () => {
     expect(true).toBe(false);
@@ -20,5 +49,4 @@ describe('Testing the router constructor', () => {
   it('Does not have a DELETE route for profile path', () => {
     expect(true).toBe(false);
   });
-
 });
