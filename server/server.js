e13b13d6c9696180c606a540e6a50fda4e582a11
Micha Davis <m.leigh.davis@gmail.com>
2022-05-24 17:56:23 -0700

    add graph route to app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const { profileCollection, storyCollection } = require('./models');
+const { profileCollection, storyCollection, adjacencyCollection } = require('./models');
 const routify = require('./routify');
 const authRouter = require('./auth/routes');
 const router = express.Router();
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(routify(profileCollection, 'profile', router));
 app.use(routify(storyCollection, 'story', router));
+app.use(routify(adjacencyCollection, 'graph', router));
 
 module.exports = {
   app,
