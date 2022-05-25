9c90235becdf6d6200831d34e0a4595d38990627
Micha Davis <m.leigh.davis@gmail.com>
2022-05-24 17:57:27 -0700

    add adjacency model

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 const Collection = require('./Collection');
 const profileSchema = require('./profile');
 const storySchema = require('./story');
+const adjacencySchema = require('./adjacencies');
 
 const { DATABASE_URL, NODE_ENV } = process.env;
 
@@ -18,9 +19,11 @@ const sequelize = new Sequelize(dbUrl, config);
 
 const profileCollection = new Collection(sequelize, 'profiles', profileSchema);
 const storyCollection = new Collection(sequelize, 'stories', storySchema);
+const adjacencyCollection = new Collection(sequelize, 'adjacencies', adjacencySchema);
 
 module.exports = {
   contentDb: sequelize,
   profileCollection: profileCollection,
   storyCollection: storyCollection,
+  adjacencyCollection: adjacencyCollection,
 };
