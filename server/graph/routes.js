88b085652dc70e4bbf830cdb6447e736408e987a
Micha Davis <m.leigh.davis@gmail.com>
2022-05-25 17:38:51 -0700

    refactor to single table

--- server/graph/routes.js
+++ server/graph/routes.js
@@ -3,20 +3,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { adjacencies, storyCollection } = require('../models');
+const { storyCollection } = require('../models');
 
-storyCollection.readAllInGroup = function async(group) {
-  const instances = this.model.findAll({ where: { group: group } });
+storyCollection.readAllInGroup = async function (group) {
+  const instances = await this.model.findAll({ where: { group: group } });
+
   return instances;
 };
 
 router.get('/graph/:id', async (req, res, next) => {
   try {
-    let record = await adjacencies.findOne({ where: { id: req.params.id } });
+    let record = await storyCollection.read(req.params.id);
     let subGraph = [];
-    console.log('record is ', record);
-    while (record.neighbors.length) {
-      subGraph.push(await adjacencies.findOne({ where: { id: record.neighbors.pop() } }));
+
+    while (record.dataValues.neighbors.length) {
+      subGraph.push(await storyCollection.read(record.dataValues.neighbors.pop()));
     }
     res.status(200).json(subGraph);
   } catch (error) {
@@ -28,34 +29,34 @@ router.get('/graph/:id', async (req, res, next) => {
 
 router.post('/graph', async (req, res, next) => {
   let response;
-  // console.log('model ', adjacencies);
-  try {
 
+  try {
 
     let potentialNeighborStories = await storyCollection.readAllInGroup(req.body.group);
 
+    // if (potentialNeighborStories.dataValues.neighbors.length) {
+    for (let neighborStory of potentialNeighborStories) {
+      if (neighborStory.dataValues.id !== req.body.id) {
+        if (neighborStory.dataValues.neighbors.length < 4) {
+          neighborStory.dataValues.neighbors.push(req.body.id);
 
+          response = await storyCollection.update(neighborStory.dataValues.id, neighborStory.dataValues);
 
-    console.log('potential neighbors is ', potentialNeighborStories);
-
-
-    if (potentialNeighborStories && potentialNeighborStories.length) {
-      for (let neighborStory of potentialNeighborStories) {
-        let record = await adjacencies.findAll({ where: { storyId: neighborStory.id } });
-        console.log('record is ', record);
-        if (record.neighbors.length < 4) {
-          record.neighbors.push(req.body.id);
-          response = await adjacencies.update(record, { where: { id: neighborStory.id } });
           break;
         }
       }
-      res.send(204).json(response);
-    } else {
-      console.log(req.body);
-      response = await adjacencies.create({ storyId: req.body.id, neighbors: [] });
-
-      res.send(204).json(response);
     }
+
+    res.send(204).json(response);
+
+    // } else {
+    //   console.log(req.body);
+    //   //response = await storyCollection.create({ storyId: req.body.id, neighbors: [] });
+
+    //   res.send(204).json(response);
+
+    // }
+
   } catch (error) {
     console.log(error);
     next(error);
