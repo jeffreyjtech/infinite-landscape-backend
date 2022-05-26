0e20f6cbe9ed650d1106a2aad6904819a8b078d8
Micha Davis <m.leigh.davis@gmail.com>
2022-05-26 13:12:14 -0700

    remove debug log

--- server/graph/routes.js
+++ server/graph/routes.js
@@ -36,7 +36,7 @@ router.get('/graph/:id', async (req, res, next) => {
         }
       }
     }
-    console.log(subGraph);
+
     res.status(200).json(subGraph);
   } catch (error) {
     console.error(error);
