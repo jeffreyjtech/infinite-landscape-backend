3b317f3049640a2a9f1179f428c5087c8d578f87
Micha Davis <m.leigh.davis@gmail.com>
2022-05-26 10:52:42 -0700

    add logic to handle adding a node to an empty graph add logic to handle adding a node which is the first of its group

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,6 @@ const { Sequelize, DataTypes } = require('sequelize');
 const Collection = require('./Collection');
 const profileSchema = require('./profile');
 const storySchema = require('./story');
-//const adjacencyModel = require('./adjacencies');
 
 const { DATABASE_URL, NODE_ENV } = process.env;
 
@@ -21,29 +20,33 @@ const storyCollection = new Collection(sequelize, 'stories', storySchema);
 
 storyCollection.create = async function (json) {
   let potentialNeighborStories = await this.readAllInGroup(json.group);
+  let newStory = await this.model.create(json);
 
-  if(potentialNeighborStories.length === 0) {
-    let record = await this.model.create({ ...json, neighbors: [] });
-    return record;
+  if (potentialNeighborStories.length === 0) {
+    //console.log('no neighbors for ', newStory.id);
+    potentialNeighborStories = await this.readAll();
+    if (potentialNeighborStories.length === 0) {
+      //console.log('graph is empty. returning detached node ', newStory.id);
+      return newStory;
+    }
   }
 
-  // if (potentialNeighborStories.dataValues.neighbors.length) {
   for (let neighborStory of potentialNeighborStories) {
-    if (neighborStory.dataValues.id !== json.id) {
+    //console.log('inspecting story: ', neighborStory.id);
+    if (neighborStory.dataValues.id !== newStory.id) {
+      //console.log('story', neighborStory.id, 'is not ', newStory.id, '. Inspecting neighbors: ', neighborStory.neighbors);
       if (neighborStory.dataValues.neighbors.length < 4) {
-        let newStory = await this.model.create({ ...json, neighbors: [neighborStory.dataValues.id] });
-        await this.update(neighborStory.dataValues.id, {neighbors: [...neighborStory.dataValues.neighbors, newStory.id] });
-
-        return newStory;
+        let updatedStory = await this.update(newStory.id, { neighbors: [neighborStory.dataValues.id] });
+        /*let updatedNeighbor = */await this.update(neighborStory.dataValues.id, { neighbors: [...neighborStory.dataValues.neighbors, newStory.id] });
+        // console.log('outcome: new story ', newStory.id, 'has ', updatedStory.neighbors, 'and neighbor ', neighborStory.id, 'has ', updatedNeighbor.neighbors);
+        return updatedStory;
       }
     }
   }
 };
-//const adjacencies = adjacencyModel(sequelize, DataTypes);
 
 module.exports = {
   contentDb: sequelize,
   profileCollection: profileCollection,
   storyCollection: storyCollection,
-  //adjacencies: adjacencies,
 };
