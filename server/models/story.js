7257d402e3485c1275675b9137ea461db2cce82f
Micha Davis <m.leigh.davis@gmail.com>
2022-05-25 17:39:46 -0700

    add neighbors to schema

--- server/models/story.js
+++ server/models/story.js
@@ -12,6 +12,7 @@ const storySchema = {
   group: { type: DataTypes.ENUM(genres), allowNull: false },
   color: { type: DataTypes.STRING, allowNull: false },
   tooltips: { type: DataTypes.JSON },
+  neighbors: { type: DataTypes.ARRAY(DataTypes.INTEGER), allowNull: false },
 };
 
 module.exports = storySchema;
