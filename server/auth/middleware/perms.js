4a177f76084d1d7528fcf0895752b4bd37fa188b
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-06-02 00:35:26 -0700

    Adjusted perms error handling again, so 404 is not overwritten

--- server/auth/middleware/perms.js
+++ server/auth/middleware/perms.js
@@ -6,14 +6,14 @@ module.exports = (collection) => async (req, res, next) => {
   try {
     const foundContent = await collection.read(req.params.id);
 
-    if (!foundContent) throw errorWithStatus('Resource not found', 404);
+    if (!foundContent) next(errorWithStatus('Resource not found', 404));
 
     // req.user has all the user info from bearerAuth middleware
     // Including the user's verified role and username
     if (req.user.username === foundContent.username || req.user.role === 'admin') {
       next();
     } else {
-      throw errorWithStatus('You cannot modify another user\'s content', 403);
+      throw new Error();
     }
   } catch (error) {
     console.error(error);
