ba14ce9b97072765f266e21fb87048f7d66e9db8
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-06-01 23:08:54 -0700

    Renamed variable for clarity

--- server/auth/middleware/perms.js
+++ server/auth/middleware/perms.js
@@ -4,13 +4,13 @@ const errorWithStatus = require('../../error/ErrorWithStatus');
 
 module.exports = (collection) => async (req, res, next) => {
   try {
-    const foundMessage = await collection.read(req.params.id);
+    const foundContent = await collection.read(req.params.id);
 
-    if (!foundMessage) throw errorWithStatus('Resource not found', 404);
+    if (!foundContent) throw errorWithStatus('Resource not found', 404);
 
     // req.user has all the user info from bearerAuth middleware
     // Including the user's verified role and username
-    if (req.user.username === foundMessage.username || req.user.role === 'admin') {
+    if (req.user.username === foundContent.username || req.user.role === 'admin') {
       next();
     } else {
       next(errorWithStatus('You cannot modify another user\'s content', 403));
