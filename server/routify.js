ac3ac12c6062c0fa673b20fdd1478ba184af3215
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-06-24 00:30:26 -0700

    Refactored routify to export handlers for each route

--- server/routify.js
+++ server/routify.js
@@ -21,71 +21,88 @@ const errorOnBadParam = (paramKey) => (req, res, next) => {
   }
 };
 
-module.exports = (collection, path, router) => {
-  router.post(`/${path}`, errorOnEmptyBody, bearerAuth, async (req, res, next) => {
-    try {
-      let record = await collection.create(req.body);
-      res.status(201).json(record);
-    } catch (e) {
-      console.error(e);
-      next(e);
-    }
-  });
+const postHandler = (collection) => async (req, res, next) => {
+  try {
+    let record = await collection.create(req.body);
+    res.status(201).json(record);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+};
 
-  router.get(`/${path}`, async (req, res, next) => {
-    try {
-      let records = await collection.readAll();
-      res.status(200).json(records);
-    } catch (e) {
-      console.error(e);
-      next(e);
-    }
-  });
+const getHandler = (collection) => async (req, res, next) => {
+  try {
+    let records = await collection.readAll();
+    res.status(200).json(records);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+};
 
-  router.get(`/${path}/:id`, errorOnBadParam('id'), async (req, res, next) => {
-    try {
-      let record = await collection.read(req.params.id);
-      res.status(200).json(record);
-    } catch (e) {
-      console.error(e);
-      next(e);
-    }
-  });
+const getIdHandler = (collection) => async (req, res, next) => {
+  try {
+    let record = await collection.read(req.params.id);
+    res.status(200).json(record);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+};
 
-  router.put(
-    `/${path}/:id`,
-    errorOnBadParam('id'),
-    errorOnEmptyBody,
-    bearerAuth,
-    perms(collection),
-    async (req, res, next) => {
-      try {
-        let record = await collection.update(req.body, req.params.id);
-        res.status(200).json(record);
-      } catch (e) {
-        console.error(e);
-        next(e);
-      }
-    },
-  );
+const putHandler = (collection) => async (req, res, next) => {
+  try {
+    let record = await collection.update(req.body, req.params.id);
+    res.status(200).json(record);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+};
 
-  if (path !== 'profile') {
-    router.delete(
+const deleteHandler = (collection) => async (req, res, next) => {
+  try {
+    let record = await collection.delete(req.params.id);
+    res.status(200).json(record);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+};
+
+module.exports = {
+  routify: (collection, path, router) => {
+    router.post(`/${path}`, errorOnEmptyBody, bearerAuth, postHandler(collection));
+
+    router.get(`/${path}`, getHandler(collection));
+
+    router.get(`/${path}/:id`, errorOnBadParam('id'), getIdHandler(collection));
+
+    router.put(
       `/${path}/:id`,
       errorOnBadParam('id'),
+      errorOnEmptyBody,
       bearerAuth,
       perms(collection),
-      async (req, res, next) => {
-        try {
-          let record = await collection.delete(req.params.id);
-          res.status(200).json(record);
-        } catch (e) {
-          console.error(e);
-          next(e);
-        }
-      },
+      putHandler(collection),
     );
-  }
 
-  return router;
+    if (path !== 'profile') {
+      router.delete(
+        `/${path}/:id`,
+        errorOnBadParam('id'),
+        bearerAuth,
+        perms(collection),
+        deleteHandler(collection),
+      );
+    }
+
+    return router;
+  },
+  getHandler,
+  getIdHandler,
+  postHandler,
+  putHandler,
+  deleteHandler,
 };
